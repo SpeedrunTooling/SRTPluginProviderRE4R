df2f733c902ed6568ac7df4aed1a8527ba9f82aa
Christopher D Couture <c.couture@exsi.ca>
2023-06-04 00:32:08 -0400

    deleted GameVersion file and relocated its data to GameHashes Temporarily pending more tweaks and methods to be generalized in srt host.

--- SRTPluginProviderRE4R/GameHashes.cs
+++ SRTPluginProviderRE4R/GameHashes.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
@@ -7,66 +8,90 @@ using Microsoft.Extensions.Logging;
 
 namespace SRTPluginProducerRE4R
 {
-    /// <summary>
-    /// SHA256 hashes for the RE4 REmake game executables.
-    /// 
-    /// Resident Evil 4 (WW): https://steamdb.info/app/2050650/ / https://steamdb.info/depot/2050651/
-    /// Biohazard 4 (CERO Z): ??? / ???
-    /// </summary>
+	/// <summary>
+	/// SHA256 hashes for the RE4 REmake game executables.
+	/// 
+	/// Resident Evil 4 (WW): https://steamdb.info/app/2050650/ / https://steamdb.info/depot/2050651/
+	/// Biohazard 4 (CERO Z): ??? / ???
+	/// </summary>
     public static class GameHashes
     {
-        private static readonly byte[] re4rWW_11025382 = new byte[32] { 0x3E, 0xC3, 0x90, 0x1D, 0xD4, 0xF7, 0x0C, 0x7E, 0x03, 0xDB, 0xE3, 0x13, 0xA4, 0x7A, 0xDB, 0xC1, 0x10, 0x94, 0xD9, 0xAE, 0x34, 0xB6, 0xF3, 0x5E, 0xF9, 0xA0, 0x8C, 0x4F, 0x57, 0x32, 0x6D, 0x1E };
-        private static readonly byte[] re4rWW_20230407_1 = new byte[32] { 0x7A, 0x0A, 0x58, 0xB6, 0x00, 0x35, 0xCF, 0xBB, 0x73, 0xDD, 0x4F, 0x22, 0x16, 0x38, 0xD2, 0x6E, 0x50, 0xC8, 0xBF, 0xE6, 0xFC, 0x68, 0x77, 0x6B, 0x1F, 0x05, 0x1F, 0x33, 0x4A, 0x10, 0x00, 0x5C };
-        private static readonly byte[] re4rWW_20230323_1 = new byte[32] { 0x3C, 0x81, 0x07, 0xE9, 0x35, 0x2D, 0x0C, 0x4F, 0x39, 0x3D, 0x37, 0x50, 0xF0, 0xAC, 0x86, 0x62, 0x39, 0x1D, 0x52, 0x55, 0x9E, 0x94, 0xB5, 0x86, 0x73, 0x70, 0x50, 0xE0, 0xC5, 0x5E, 0xC3, 0x18 };
-        private static readonly byte[] re4rDEMO_20230309_1 = new byte[32] { 0xBA, 0x9D, 0xC7, 0x4A, 0xC8, 0x52, 0x30, 0x62, 0x12, 0xB2, 0xDD, 0x17, 0x93, 0xA8, 0xB6, 0xFD, 0x09, 0x2A, 0x62, 0xD5, 0x03, 0xFC, 0x87, 0x30, 0x59, 0xD2, 0x62, 0x50, 0xAE, 0x73, 0x0E, 0xAC };
+		/// <summary>
+		/// Generates a version log file containing a C# byte array declaration representing the version information of a game.
+		/// </summary>
+		/// <param name="logger">The logger instance for logging informational messages.</param>
+		/// <param name="cs">The byte array containing the version information.</param>
+		/// <param name="gameName">The name of the game.</param>
+		private static void OutputVersionString(ILogger<SRTPluginProducerRE4R> logger, byte[] cs, string gameName)
+		{
+			// Create a StringBuilder instance to build the version string
+			StringBuilder sb = new StringBuilder($"private static readonly byte[] {gameName.ToLowerInvariant()}??_00000000 = new byte[{cs.Length}] {{ ");
 
-        private static void OutputVersionString(ILogger<SRTPluginProducerRE4R> logger, byte[] cs)
-        {
-            StringBuilder sb = new StringBuilder($"private static readonly byte[] re4r??_00000000 = new byte[{cs.Length}] {{ ");
+			// Iterate over the byte array 'cs' and append each byte to the StringBuilder
+			for (int i = 0; i < cs.Length; i++)
+				sb.AppendFormat("0x{0:X2}, ", cs[i]);
 
-            for (int i = 0; i < cs.Length; i++)
-            {
-                sb.AppendFormat("0x{0:X2}, ", cs[i]);
-            }
-            sb.Length -= 2;
-            sb.Append(" };");
+			// Remove the extra trailing comma and space
+			sb.Length -= 2;
 
-            string filename = "version.log";
-            logger.LogInformation($"Please message {new PluginInfo().Author} with the {filename} file.");
-            // write output to file
-            using (StreamWriter writer = new StreamWriter(filename))
-                writer.WriteLine(sb.ToString());
-        }
+			// Append the closing bracket and semicolon to complete the version string
+			sb.Append(" };");
 
-        public static GameVersion DetectVersion(ILogger<SRTPluginProducerRE4R> logger, string? filePath)
-        {
-            if (string.IsNullOrWhiteSpace(filePath))
-            {
-                logger.LogError($"Unknown Version: string.IsNullOrWhiteSpace({nameof(filePath)}) returned true.");
-                return GameVersion.Unknown;
-            }
+			// Create a log file name based on the lowercased 'gameName'
+			string filename = $"{gameName.ToLowerInvariant()}_version.log";
 
-            byte[] checksum;
-            using (SHA256 hashFunc = SHA256.Create())
-            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete))
-                checksum = hashFunc.ComputeHash(fs);
+			// Log an informational message indicating the author of the plugin and the log file name
+			logger.LogInformation($"Please message {new PluginInfo().Author} with the {filename} file.");
 
-            OutputVersionString(logger, checksum); // TESTING!!
+			// Open the log file for writing and write the version string to it
+			using (StreamWriter writer = new StreamWriter(filename))
+				writer.WriteLine(sb.ToString());
+		}
 
-            if (checksum.SequenceEqual(re4rWW_11025382))
-                return GameVersion.RE4R_WW_11025382;
-            else if (checksum.SequenceEqual(re4rWW_20230407_1))
-                return GameVersion.RE4R_WW_20230407_1;
-            else if (checksum.SequenceEqual(re4rWW_20230323_1))
-                return GameVersion.RE4R_WW_20230323_1;
-            else if (checksum.SequenceEqual(re4rDEMO_20230309_1))
-                return GameVersion.RE4R_Demo_20230309_1;
-            else
-            {
-                logger.LogWarning("Unknown Version"); // TODO: Replace all Console., Trace., Debug. with logger.
-                OutputVersionString(logger, checksum);
-                return GameVersion.Unknown;
-            }
-        }
-    }
+		/// <summary>
+		/// Detects the version of a game based on its file checksum and a dictionary of supported versions.
+		/// </summary>
+		/// <typeparam name="VersionType">The type representing the game version.</typeparam>
+		/// <param name="logger">The logger instance for logging informational, error, and warning messages.</param>
+		/// <param name="filePath">The path to the game file.</param>
+		/// <param name="_supportedVersions">A dictionary containing byte array checksums and their corresponding versions.</param>
+		/// <returns>The detected version of the game, or null if the version is unknown or not supported.</returns>
+		public static VersionType? DetectVersion<VersionType>(ILogger<SRTPluginProducerRE4R> logger, string? filePath, Dictionary<byte[], VersionType> _supportedVersions)
+		{
+			// Check if the file path is null, empty, or contains only whitespace
+			if (string.IsNullOrWhiteSpace(filePath))
+			{
+				logger.LogError($"Unknown Version: string.IsNullOrWhiteSpace({nameof(filePath)}) returned true.");
+				return default;
+			}
+
+			byte[] checksum;
+			// Calculate the SHA256 checksum of the game file
+			using (SHA256 hashFunc = SHA256.Create())
+			using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete))
+				checksum = hashFunc.ComputeHash(fs);
+
+			// Iterate through the supported versions dictionary and compare the checksums
+			foreach (var version in _supportedVersions)
+			{
+				if (checksum.SequenceEqual(version.Key))
+					return _supportedVersions[version.Key];
+			}
+
+			// The version is unknown or not supported
+			logger.LogWarning("Unknown Version");
+			// Output the version checksum in a log file for further analysis
+			OutputVersionString(logger, checksum, "re4r");
+			return default;
+		}
+	}
+
+	public enum GameVersion : int
+	{
+		Unknown,
+		RE4R_Demo_20230309_1,
+		RE4R_WW_20230323_1,
+		RE4R_WW_20230407_1,
+		RE4R_WW_11025382,
+	}
 }
