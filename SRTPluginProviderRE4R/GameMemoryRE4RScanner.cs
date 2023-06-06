1dfbd5f032ab1748128abdb1bbe0c50ea1566b24
Christopher D Couture <c.couture@exsi.ca>
2023-06-06 01:11:35 -0400

    latest provider and overlay updates

--- SRTPluginProviderRE4R/GameMemoryRE4RScanner.cs
+++ SRTPluginProviderRE4R/GameMemoryRE4RScanner.cs
@@ -427,4 +427,13 @@ namespace SRTPluginProducerRE4R
         }
         #endregion
     }
+
+    public enum GameVersion : int
+    {
+        Unknown,
+        RE4R_Demo_20230309_1,
+        RE4R_WW_20230323_1,
+        RE4R_WW_20230407_1,
+        RE4R_WW_11025382,
+    }
 }
